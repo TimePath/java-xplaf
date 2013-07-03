319a72ba1b8d4bd6e03e11b445e6b4650e6d0f7b
TimePath <andrew.hardaker1995@gmail.com>
2013-07-03 21:07:52 +1000

    Custom icons for directory trees

--- src/com/timepath/swing/DirectoryTreeCellRenderer.java
+++ src/com/timepath/swing/DirectoryTreeCellRenderer.java
@@ -1,8 +1,11 @@
 package com.timepath.swing;
 
+import com.timepath.io.utils.ViewableData;
 import java.awt.Component;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 /**
@@ -12,11 +15,29 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 @SuppressWarnings("serial")
 public class DirectoryTreeCellRenderer extends DefaultTreeCellRenderer {
 
+    private JLabel label = new JLabel();
+
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel,
                                                   boolean expanded, boolean leaf, int row,
                                                   boolean hasFocus) {
-        return super.getTreeCellRendererComponent(tree, value, sel, sel, false, row, hasFocus);
+        Component comp = super.getTreeCellRendererComponent(tree, value, sel, sel, false, row,
+                                                            hasFocus);
+        if(comp instanceof JLabel) {
+            label = (JLabel) comp;
+            if(value instanceof DefaultMutableTreeNode) {
+                DefaultMutableTreeNode dmtn = (DefaultMutableTreeNode) value;
+                if(dmtn.getUserObject() instanceof ViewableData) {
+                    ViewableData data = (ViewableData) dmtn.getUserObject();
+                    label.setIcon(null);
+                    label.setIcon(data.getIcon());
+                    label.setText(data.toString());
+                    return label;
+                }
+            }
+        }
+        return comp;
+
     }
 
     private static final Logger LOG = Logger.getLogger(DirectoryTreeCellRenderer.class.getName());
