e2f8d5897c29ab6ca25dcb6836fd6aa1687e6e7b
TimePath <andrew.hardaker1995@gmail.com>
2013-07-04 22:48:38 +1000

    RandomAccessFile wrapper

--- src/com/timepath/DataUtils.java
+++ src/com/timepath/DataUtils.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
@@ -69,7 +68,7 @@ public class DataUtils {
     }
 
     public static ByteBuffer mapFile(File f) throws IOException {
-        RandomAccessFile raf = new RandomAccessFile(f, "r");
+        java.io.RandomAccessFile raf = new java.io.RandomAccessFile(f, "r");
 //        FileInputStream fis = new FileInputStream(f);
         FileChannel fc = raf.getChannel();
         MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 0, f.length());
@@ -159,119 +158,6 @@ public class DataUtils {
 //        LOG.info(Arrays.toString(baos.toByteArray()) + "");
         return Charset.forName("UTF-8").decode(ByteBuffer.wrap(baos.toByteArray())).toString();
     }
-
-    //<editor-fold defaultstate="collapsed" desc="Old stuff">
-    public static byte readByte(RandomAccessFile f) throws IOException {
-        byte b = f.readByte();
-        return b;
-    }
-
-    public static int readUByte(RandomAccessFile f) throws IOException {
-        return readByte(f) & 0xff;
-    }
-
-    public static byte[] readBytes(RandomAccessFile f, int num) throws IOException {
-        byte[] arr = new byte[num];
-        f.read(arr);
-        return arr;
-    }
-
-    public static char readLEChar(RandomAccessFile f) throws IOException {
-        return (char) (readUByte(f) + (readUByte(f) << 8));
-    }
-
-    public static void writeLEChar(RandomAccessFile f, int v) throws IOException {
-        f.writeByte((byte) (0xff & v));
-        f.writeByte((byte) (0xff & (v >> 8)));
-    }
-
-    public static void writeLEChars(RandomAccessFile f, String s) throws IOException {
-        for(int i = 0; i < s.length(); i++) {
-            writeLEChar(f, s.charAt(i));
-        }
-    }
-
-    /**
-     * aka WORD
-     *
-     * @param f
-     *
-     * @return
-     *
-     * @throws IOException
-     */
-    public static short readLEShort(RandomAccessFile f) throws IOException {
-        return (short) (readByte(f) + (readByte(f) << 8));
-    }
-
-    public static void writeLEShort(RandomAccessFile f, short value) throws IOException {
-        f.writeByte(value);
-        f.writeByte(value >> 8);
-    }
-
-    public static int readULEShort(RandomAccessFile f) throws IOException {
-        return readUByte(f) + (readUByte(f) << 8);
-    }
-
-    public static void writeULEShort(RandomAccessFile f, short value) throws IOException {
-        f.writeByte(value & 0xFF);
-        f.writeByte((value >> 8) & 0xFF);
-    }
-
-    /**
-     * aka DWORD
-     *
-     * @param f
-     *
-     * @return
-     *
-     * @throws IOException
-     */
-    public static int readLEInt(RandomAccessFile f) throws IOException {
-        return readByte(f) + (readByte(f) << 8) + (readByte(f) << 16) + (readByte(f) << 24);
-    }
-
-    public static int readInt(RandomAccessFile f) throws IOException {
-        return (readUByte(f) << 24) + (readUByte(f) << 16) + (readUByte(f) << 8) + readUByte(f);
-    }
-
-    public static int readULEInt(RandomAccessFile f) throws IOException {
-        return readUByte(f) + (readUByte(f) << 8) + (readUByte(f) << 16) + (readUByte(f) << 24);
-    }
-
-    public static void writeLEInt(RandomAccessFile f, int value) throws IOException {
-        //        ByteBuffer buffer = ByteBuffer.allocate(1000);
-        //        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        //        buffer.putInt(i);
-        //        f.write(buffer.array());
-        f.writeByte(value & 0xFF);
-        f.writeByte((value >> 8) & 0xFF);
-        f.writeByte((value >> 16) & 0xFF);
-        f.writeByte((value >> 24) & 0xFF);
-    }
-
-    /**
-     * aka DWORD
-     *
-     * @param f
-     *
-     * @return
-     *
-     * @throws IOException
-     */
-    public static int readULong(RandomAccessFile f) throws IOException {
-        return readUByte(f) + (readUByte(f) << 8) + (readUByte(f) << 16) + (readUByte(f) << 24);
-    }
-
-    public static void writeULong(RandomAccessFile f, int value) throws IOException {
-        writeLEInt(f, value);
-    }
-
-    public static float readLEFloat(RandomAccessFile f) throws IOException {
-        int intBits = readUByte(f) + (readUByte(f) << 8) + (readUByte(f) << 16) + (readUByte(f) << 24);
-        return Float.intBitsToFloat(intBits);
-    }
-    //</editor-fold>
 
     public static String toBinaryString(short n) {
         return toBinaryString(n, 16);
